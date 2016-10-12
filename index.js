4af5017805c2876a4013ff65d6245cda479912ce
Dave Seidman <dave.seidman@b-reel.com>
2016-10-12 17:42:04 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var apiKey = require('./.private/apikey.json'),
+    gt = require('google-translate')(apiKey.key);
+
+
+gt.translate('My name is Brandon', 'en', 'es', (err, translation) => {
+
+    console.log(translation);
+    // =>  Mi nombre es Brandon
+});
