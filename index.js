474ef5e2713ac178e3020176ae4da7f312ffa916
Dave Seidman <dave.seidman@b-reel.com>
2016-10-12 17:50:22 -0400

    starting simple http server

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var apiKey = require('./private/apikey.json'),
-    gt = require('google-translate')(apiKey.key);
+    gt = require('google-translate')(apiKey.key),
+    http = require('http');
+
+const PORT = 80;
+
+function handleRequest(request, responst) {
+
+    response.end("it worked" + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+
+    console.log("server listening on port", PORT);
+})
 
 
-gt.translate('My name is Brandon', 'en', 'es', (err, translation) => {
 
-    console.log(translation);
-    // =>  Mi nombre es Brandon
-});
+
+// gt.translate('My name is Brandon', 'en', 'es', (err, translation) => {
+//
+//     console.log(translation);
+//     // =>  Mi nombre es Brandon
+// });
